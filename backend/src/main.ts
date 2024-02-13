cf6bdda3e68a8051ea1e28ea97dcf255cff36b8c
Márcio Gabriel <gpcgabriel0@gmail.com>
2024-02-13 19:57:06 -0300

    feat: update port to env value

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
-    await app.listen(3000);
-    console.log('Listening on port 3000');
+    const port = process.env.PORT || 3001;
+    await app.listen(port, () => {
+        console.log('Server listening on port ' + port);
+    });
 }
 bootstrap();
