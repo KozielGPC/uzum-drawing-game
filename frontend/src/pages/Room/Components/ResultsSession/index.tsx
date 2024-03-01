4931ff629c42bef48e02b2008a4c5b0a9de88a80
Márcio Gabriel <gpcgabriel0@gmail.com>
2024-02-29 23:10:18 -0300

    feat: update results session

--- frontend/src/pages/Room/Components/ResultsSession/index.tsx
+++ frontend/src/pages/Room/Components/ResultsSession/index.tsx
@@ -1,67 +1,109 @@
-import { useEffect, useState } from 'react';
-import { EndMatch, MatchRounds, Round } from '../../../../interfaces/iMatch';
-import ShowDraw from '../../../../components/ShowDraw';
+import { useEffect, useState, useContext } from 'react';
+import { EndMatch, Round } from '../../../../interfaces/iMatch';
+import { ShowDraw } from '../../../../components/ShowDraw';
 import { socket } from '../../../../providers/socket';
-import { Typography } from 'antd';
+import { Card, Typography } from 'antd';
+import { Room } from '../../../../interfaces/iRoom';
+import { User } from '../../../../interfaces/iUser';
+import { UserContext } from '../../../../context/UserContext';
 
 const { Title } = Typography;
-export const ResultsSession = () => {
+export function ResultsSession() {
+    const { user, room }: { user: User | null; room: Room | null } = useContext(UserContext);
+
     const [activeResult, setActiveResult] = useState(0);
-    const [results, setResults] = useState<MatchRounds[]>([]);
-    const [secondaryResults, setSecondaryResults] = useState<Round[]>([]);
+    const [matches, setMatches] = useState<EndMatch[]>([]);
+    const [rounds, setRounds] = useState<Round[]>([]);
+    const [showAdm, setShowAdm] = useState(false);
 
-    const [cu, setCu] = useState<string[]>([]);
+    function restartGame() {
+        socket.emit('restartGame', 'macaco');
+        setShowAdm(false);
+    }
 
-    // logic to show game sequence
+    function emitNext() {
+        socket.emit('addShowRound');
+    }
+
     useEffect(() => {
-        if (results.length !== 0) {
-            setActiveResult(1);
-            if (secondaryResults.length !== results[0].rounds.length) {
-                const nextround = results[0].rounds[secondaryResults.length];
-                setSecondaryResults((secondaryResults: Round[]) => [...secondaryResults, nextround]);
-            } else {
-                results.splice(0, 1);
-                setResults(results);
-                setSecondaryResults([]);
-                if (results.length === 0) {
-                    setActiveResult(0);
-                }
-            }
+        let isAdm = false;
+        if (room && room.room_adm_id === user?.id) {
+            isAdm = true;
+        } else {
+            isAdm = false;
         }
-    }, [cu]);
 
-    useEffect(() => {
         socket.on('endMatch', (data: EndMatch) => {
-            setResults((results: MatchRounds[]) => [...results, data.rounds]);
+            if (isAdm === true) {
+                setShowAdm(true);
+                setMatches((matches: EndMatch[]) => [...matches, data]);
+            }
         });
 
-        socket.on('showNext', (data: any) => {
-            setActiveResult(1);
-            setCu([...cu, 'macaco']);
+        socket.on('showNext', () => {
+            console.log('Recebeu show next');
+            console.log('matches: ', matches);
+
+            if (matches.length !== 0) {
+                setActiveResult(1);
+                if (rounds.length !== matches[0].rounds.length) {
+                    const nextround = matches[0].rounds[rounds.length];
+                    setRounds((rounds: Round[]) => [...rounds, nextround]);
+                } else {
+                    matches.splice(0, 1);
+                    setMatches(matches);
+                    setRounds([]);
+                }
+            } else {
+                setActiveResult(0);
+            }
         });
+
+        // const handleBeforeUnload = (event: any) => {
+        //     logoff({ user_id: user?.id ?? '' });
+        // };
+
+        // window.addEventListener('beforeunload', handleBeforeUnload);
+
+        // return () => {
+        //     window.removeEventListener('beforeunload', handleBeforeUnload);
+        // };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
-        <div>
-            {activeResult === 0
-                ? null
-                : secondaryResults.length > 0
-                ? secondaryResults.map((result) =>
-                      result.type === 'draw' ? (
-                          <div>
-                              <Title level={2}>{result.sender.username + ': '}</Title>
-                              <ShowDraw draw={result.content} />
-                          </div>
-                      ) : (
-                          <div>
+        <Card title="Results" style={{ margin: '20px' }}>
+            {showAdm ? (
+                <div className="object">
+                    <h1>You have {matches.length} game to show</h1>
+                    {matches.length === 0 ? (
+                        <button onClick={() => restartGame()}>New game</button>
+                    ) : (
+                        <button onClick={() => emitNext()}>Show next</button>
+                    )}
+                </div>
+            ) : null}
+            <div>
+                {activeResult === 0
+                    ? null
+                    : rounds.length > 0
+                    ? rounds.map((result) =>
+                          result.type === 'draw' ? (
                               <div>
                                   <Title level={2}>{result.sender.username + ': '}</Title>
-                                  <Title level={2}>{result.content}</Title>
+                                  <ShowDraw draw={result.content} />
                               </div>
-                          </div>
-                      ),
-                  )
-                : null}
-        </div>
+                          ) : (
+                              <div>
+                                  <div>
+                                      <Title level={2}>{result.sender.username + ': '}</Title>
+                                      <Title level={2}>{result.content}</Title>
+                                  </div>
+                              </div>
+                          ),
+                      )
+                    : null}
+            </div>
+        </Card>
     );
-};
+}
