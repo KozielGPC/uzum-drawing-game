f029116e4e4f4dc8b6c171c3e4355c1ae86d314c
Márcio Gabriel <gpcgabriel0@gmail.com>
2024-03-08 11:53:25 -0300

    feat: test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
     const port = process.env.PORT || 3001;
+    console.log('teste');
+    
     await app.listen(port, () => {
         console.log('Server listening on port ' + port);
     });
